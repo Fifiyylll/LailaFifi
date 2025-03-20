d405aaf0154078de216975cf6bca35d685302503
Fifiii <horiiillyy@gmail.com>
2025-03-20 07:11:11 +0700

    Add files via upload

---
+++ skenario4.php
@@ -0,0 +1,9 @@
+<?php
+for ($i = 30; $i >1; $i--){
+    echo "Anak ayam turun $i, mati satu tinggal ".($i - 1);
+    echo "<br>";
+}
+echo "Anak ayam turun $i, mati satu tinggal induknya.";
+
+echo "<br>";
+?>
